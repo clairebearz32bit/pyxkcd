70e5ad653ac799a52a5847b6ab36146e55391075
drunkpolishbear <donaldtgina@gmail.com>
2020-09-06 14:30:35 -0600

    Added flag to automatically open the picture.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import argparse
 
 from shutil import copyfileobj
+from PIL import Image
 
 MONTHS = {
     '1': 'January',
@@ -19,12 +20,17 @@ MONTHS = {
 }
 
 
+def get_comic_info(comic_number):
+    return requests.get(f'https://xkcd.com/{comic_number}/info.0.json').json()
+
+
 class xkcd:
     main_uri = 'https://xkcd.com/{}/info.0.json'
     img_link = 'https://imgs.xkcd.com/comics/{}.png'
 
-    def __init__(self, comic_number, comic_title):
+    def __init__(self, comic_number, comic_title, outfile):
         self.comic_number = comic_number
+        self.outfile = outfile
 
         try:
             self.comic_title = comic_title.lower().replace(' ', '-')
@@ -45,12 +51,11 @@ class xkcd:
 
         return r
 
-    def output_comic(self, outfile):
+    def output_comic(self):
         comic = self.get_comic()
-        title = self.get_comic_info()['title'].lower().replace(' ', '-')
 
-        if outfile:
-            with open(f'{outfile}/{title}.png', 'wb') as fp:
+        if self.outfile:
+            with open(f'{self.outfile}.png', 'wb') as fp:
                 copyfileobj(comic.raw, fp)
 
     def print_comic_info(self):
@@ -63,6 +68,10 @@ class xkcd:
 
         comic_info = f'"{title}"\n{date}\nAlt: {alt}\nLink: {link}'
         print(comic_info)
+
+    def open_comic(self):
+        image = Image.open(f'{self.outfile}.png', 'r')
+        image.show()
 
 
 def main():
@@ -79,19 +88,32 @@ def main():
     parser.add_argument('-p', '--print-info',
                         action='store_true',
                         default=False)
+    parser.add_argument('-s', '--show-comic',
+                        type=str,
+                        metavar='')
 
     args = parser.parse_args()
 
     if not (args.comic_number or args.comic_title):
         parser.error('Use of either --comic-number or --comic-title is required.')
 
-    comic = xkcd(args.comic_number, args.comic_title)
+    comic = None
 
     if args.outfile:
-        comic.output_comic(args.outfile)
+        comic = xkcd(args.comic_number, args.comic_title, args.outfile)
+        comic.output_comic()
+
+    elif args.outfile is None and args.comic_title == '':
+        comic = xkcd(args.comic_number, args.comic_title, get_comic_info(args.comic_number))
 
     if args.print_info:
         comic.print_comic_info()
+
+    if args.show_comic and args.outfile:
+        comic.open_comic()
+
+    elif args.show_comic and not args.outfile:
+        parser.error('The "output_comic" function is required to use this.')
 
 
 if __name__ == '__main__':
